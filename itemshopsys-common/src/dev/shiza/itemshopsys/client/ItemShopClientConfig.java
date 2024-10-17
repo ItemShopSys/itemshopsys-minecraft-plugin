ed88920919a1c54b0684affbc1d95bf520d764bd
rchomczyk <63915083+rchomczyk@users.noreply.github.com>
2024-10-17 18:13:28 +0200

    Add comments to clarify fields present in configuration

--- itemshopsys-common/src/dev/shiza/itemshopsys/client/ItemShopClientConfig.java
+++ itemshopsys-common/src/dev/shiza/itemshopsys/client/ItemShopClientConfig.java
@@ -1,19 +1,29 @@
 package dev.shiza.itemshopsys.client;
 
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 import eu.okaeri.configs.annotation.Exclude;
+import eu.okaeri.configs.annotation.Header;
 
+@Header("Więcej informacji / More info:")
+@Header("https://wiki.itemshopsys.com/pl/plugin")
+@Header("")
 public final class ItemShopClientConfig extends OkaeriConfig {
 
-  public static final @Exclude String NOT_CONFIGURED_VALUE = "default";
+  public static final @Exclude String NOT_CONFIGURED_VALUE = "";
 
-  public boolean debug = true;
+  @Comment("tryb debugowania / debug mode")
+  public boolean debug = false;
 
+  @Comment("id sklepu / shop id (shop_)")
   public String shopId = NOT_CONFIGURED_VALUE;
 
+  @Comment("id serwera / server id (serv_)")
   public String serverId = NOT_CONFIGURED_VALUE;
 
+  @Comment("token serwera / server token")
   public String serverToken = NOT_CONFIGURED_VALUE;
 
+  @Comment("Link do API (zostaw domyslny) / API url (dont change)")
   public String apiUrl = "https://api.itemshopsys.com/v1";
 }
